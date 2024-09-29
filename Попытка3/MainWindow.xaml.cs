732eb68c50853689a55c3dd455f3df1fe4a44cb7
V <V@KOMPUTER>
2024-09-29 20:17:28 +0400

    Добавьте файлы проекта.

---
+++ Попытка3/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using Попытка3;
+
+namespace LoginWindow
+{
+    public partial class MainWindow : Window
+    {
+        private (string Username, string Password)[] users = new (string Username, string Password)[]
+        {
+            ("log1", "pass1"),
+            ("log2", "pass2"),
+            ("log3", "pass3"),
+            ("log4", "pass4"),
+            ("log5", "pass5"),
+            ("log6", "pass6"),
+            ("log7", "pass7"),
+            ("log8", "pass8"),
+            ("log9", "pass9"),
+            ("log0", "pass0")
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Поле логина или пароля пусто. Пожалуйста, введите логин или пароль.");
+                return;
+            }
+
+            foreach (var user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    // Открываем новое окно
+                    SuccessWindow successWindow = new SuccessWindow();
+                    successWindow.Show();
+
+                    // Закрываем текущее окно
+                    this.Close();
+                    return;
+                }
+            }
+
+            MessageBox.Show("Неверный логин или пароль.");
+        }
+    }
+}
---
+++ Попытка3/SuccessWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+
+namespace Попытка3
+{
+        public partial class SuccessWindow : Window
+        {
+            public SuccessWindow()
+            {
+                InitializeComponent();
+            }
+
+    }
+    }
